2f082d6de5190850ac85c2c39ac5b417fbb06663
Donel Kia Nzambi <donel.mbambukulu@icloud.com>
2026-07-09 22:58:30 +0200

    Login Page Pushed

--- src/main/java/org/donel/taskmanagerdesktop/LoginView.java
+++ src/main/java/org/donel/taskmanagerdesktop/LoginView.java
@@ -1,6 +1,7 @@
 package org.donel.taskmanagerdesktop;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -16,42 +17,9 @@ public class LoginView extends Application {
         stage.setResizable(true);
         stage.setMinWidth(800);
         stage.setMinHeight(500);
-        VBox content = new VBox();
-        BorderPane root = new BorderPane(content);
-        root.getStyleClass().add("custom-ui-panel");
 
-        java.net.URL cssUrl = getClass().getResource("/org/donel/taskmanagerdesktop/styles.css");
-        if (cssUrl == null) {
-            throw new IllegalStateException("styles.css not found on classpath");
-        }
-        root.getStylesheets().add(cssUrl.toExternalForm());
-
-        Label title = new Label("Login");
-        title.setId("login-label");
-        title.getStyleClass().add("login-label");
-        title.setText("Login");
-
-
-        if (stage.getHeight() == Screen.getPrimary().getBounds().getHeight() & stage.getWidth() == Screen.getPrimary().getBounds().getWidth()) {
-            VBox topBox = new VBox(title);
-            topBox.setAlignment(Pos.CENTER_RIGHT);
-            topBox.setSpacing(40);
-            //root.setTop(topBox);
-            root.setCenter(topBox);
-
-            Scene scene = new Scene(root, 800, 500);
-            stage.setScene(scene);
-
-        }
-        else {
-            VBox topBox = new VBox(title);
-            topBox.setAlignment(Pos.CENTER);
-            topBox.setSpacing(10);
-            root.setTop(topBox);
-
-            Scene scene = new Scene(root, 800, 500);
-            stage.setScene(scene);
-        }
+        Scene scene = new Scene(FXMLLoader.load(getClass().getResource("LoginView.fxml")));
+        stage.setScene(scene);
 
 
 
