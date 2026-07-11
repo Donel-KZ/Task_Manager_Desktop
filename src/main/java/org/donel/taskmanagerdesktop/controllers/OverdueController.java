cdd2b7dcabd27e09472b34af137e51c404390552
Donel Kia Nzambi <donel.mbambukulu@icloud.com>
2026-07-11 03:08:41 +0200

    Created the OverdueController file

---
+++ src/main/java/org/donel/taskmanagerdesktop/controllers/OverdueController.java
@@ -0,0 +1,87 @@
+package org.donel.taskmanagerdesktop.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+
+import java.io.IOException;
+import java.util.List;
+
+public class OverdueController {
+    @FXML
+    private StackPane contentArea;
+    @FXML
+    private Button homeButton;
+
+    @FXML
+    private Button pending;
+    @FXML
+    private Button finished;
+    @FXML
+    private Button overdue;
+    @FXML
+    private Button groupProjects;
+    @FXML
+    private Button settings;
+
+    @FXML
+    public void initialize() throws IOException {
+        showoverdue();
+    }
+
+    private void showfinished() throws IOException {
+        loadScreen("FinishedView.fxml", finished);
+    }
+
+    @FXML
+    private void showPending() throws IOException {
+        loadScreen("PendindView.fxml", pending);
+    }
+
+
+    @FXML
+    private void showoverdue() throws IOException {
+        loadScreen("OverdueView.fxml", overdue);
+    }
+
+    @FXML
+    private void groupProjects() throws IOException {
+        loadScreen("GroupProjectView.fxml", groupProjects);
+    }
+
+    @FXML
+    private void settings() throws IOException {
+        loadScreen("SettingsView.fxml", settings);
+    }
+
+    @FXML
+    private void showCalender() throws IOException {
+        loadScreen("CalenderView.fxml", finished);
+    }
+
+
+    @FXML
+    private void loadScreen(String s, Button button) throws IOException {
+        Parent loader = FXMLLoader.load(getClass().getResource(s));
+        contentArea.getChildren().add(loader);
+        setActiveButton(button);
+    }
+
+    private void setActiveButton(Button activeButton) {
+        List<Button> navbuttons = List.of(homeButton, pending, finished, overdue, groupProjects, settings);
+
+        for (Button button1 : navbuttons) {
+            button1.getStyleClass().remove("nav-button-active");
+            if (!button1.getStyleClass().contains("nav-button")) {
+                button1.getStyleClass().add("nav-button");
+            }
+        }
+        activeButton.getStyleClass().remove("nav-button");
+        activeButton.getStyleClass().add("nav-button-active");
+
+
+    }
+
+}
